787f787c36858a3b61e49595293ee1892c818dea
romiyusnandar <yusromi04@gmail.com>
2025-04-08 02:49:28 +0000

    power: smb5-lib: Implement bypass charging
    Using patch by Rve27 as reference
    
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -56,6 +56,7 @@
 bool skip_thermal = false;
 module_param(skip_thermal, bool, 0644);
 
+static int bypass_charging = 0;
 static void update_sw_icl_max(struct smb_charger *chg, int pst);
 static int smblib_get_prop_typec_mode(struct smb_charger *chg);
 static int smblib_set_sw_conn_therm_regulation(struct smb_charger *chg, bool enable);
@@ -2322,9 +2323,14 @@ static void smblib_get_start_vbat_before_step_charge(struct smb_charger *chg)
 int smblib_get_prop_input_suspend(struct smb_charger *chg,
 				  union power_supply_propval *val)
 {
-	val->intval
-		= (get_client_vote(chg->usb_icl_votable, USER_VOTER) == 0)
-		 && get_client_vote(chg->dc_suspend_votable, USER_VOTER);
+	if ((get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1)) {
+    val->intval = 1;
+  } else if (bypass_charging) {
+    val->intval = 2;
+  } else {
+    val->intval = 0;
+  }
+
 	return 0;
 }
 
@@ -2899,14 +2905,14 @@ int smblib_set_prop_input_suspend(struct smb_charger *chg,
 	dump_stack();
 
 	/* vote 0mA when suspended */
-	rc = vote(chg->usb_icl_votable, USER_VOTER, (bool)val->intval, 0);
+	rc = vote(chg->usb_icl_votable, USER_VOTER, false, 0);
 	if (rc < 0) {
 		smblib_err(chg, "Couldn't vote to %s USB rc=%d\n",
 			(bool)val->intval ? "suspend" : "resume", rc);
 		return rc;
 	}
 
-	rc = vote(chg->dc_suspend_votable, USER_VOTER, (bool)val->intval, 0);
+	rc = vote(chg->dc_suspend_votable, USER_VOTER, false, 0);
 	if (rc < 0) {
 		smblib_err(chg, "Couldn't vote to %s DC rc=%d\n",
 			(bool)val->intval ? "suspend" : "resume", rc);
@@ -2915,6 +2921,23 @@ int smblib_set_prop_input_suspend(struct smb_charger *chg,
 
 	if (chg->use_bq_pump)
 		chg->bq_input_suspend = !!(val->intval);
+
+  if (val->intval == 1) {
+    rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 1, 0);
+    bypass_charging = 0;
+  } else if (val->intval == 2) {
+    rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+    bypass_charging = 1;
+  } else {
+    rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+    bypass_charging = 0;
+  }
+
+  if (rc < 0) {
+    smblib_err(chg, "Couldn't vote to %d input_suspend rc=%d\n",
+      val->intval, rc);
+    return rc;
+  }
 
 	power_supply_changed(chg->batt_psy);
 	return rc;
@@ -3240,6 +3263,7 @@ static void smblib_thermal_setting_work(struct work_struct *work)
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+  int system_temp_level = 0;
 	int rc;
         union power_supply_propval batt_temp ={0,};
 
@@ -3298,7 +3322,27 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
 			chg->thermal_mitigation[chg->system_temp_level]);
 #endif
-	return 0;
+
+  if (get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1) {
+    pr_info("%s bypass charging enabled",__FUNCTION__);
+    return vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, true, 0);
+  }
+
+  if (bypass_charging) {
+    if (chg->thermal_levels - 2 > system_temp_level) system_temp_level = chg->thermal_levels-2;
+    if (system_temp_level < 0) system_temp_level = 0;
+    pr_info("%s limited charging enabled %d",__FUNCTION__, system_temp_level);
+  } else if (system_temp_level > 0) {
+    pr_info("%s charging enabled, but thermal limited %d",__FUNCTION__, system_temp_level);
+  }
+
+  if (system_temp_level == 0)
+    return vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, false, 0);
+
+  vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
+    chg->thermal_mitigation[system_temp_level]);
+    
+  return 0;
 }
 
 int smblib_set_prop_input_current_limited(struct smb_charger *chg,
